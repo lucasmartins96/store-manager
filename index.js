2450714978c913da561871998129cecabb364dd7
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-09-09 23:17:22 -0300

    complementando o index

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const productsRouter = require('./routers/productsRoute');
+
+const PORT = 3000;
+const app = express();
+app.use(express.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter);
+
+app.listen(PORT, () => console.log('server running on port 3000'));
